8e25ab2df327aae615b5cf8589cacb4e3d263894
PCPCRACK <127131700+PCPCRACK@users.noreply.github.com>
2023-03-21 13:50:13 -0500

    Update primero.py
    xd
    https://i.postimg.cc/MpCnx1gL/descarga.jpg

--- primero.py
+++ primero.py
@@ -4,5 +4,5 @@ while bandera or num != 0:
   bandera = False
   num = int(input("Ingrese un entero o  0 para salir: "))
   sum += num
-  print("La suma de los numeros ingresados es " + str(sum))
+  print("La suma de los números ingresados es " + str(sum))
 print("Ciclo terminado")
